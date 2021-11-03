a410ee538173123854ed48305b927c064acf7338
Mnarvaez98 <mnarvaez24@uan.edu.co>
2021-11-03 11:08:45 -0500

    fixed/ undefined variable inicialized

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-app = express();
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/voces", require("./src/routes/voces"));

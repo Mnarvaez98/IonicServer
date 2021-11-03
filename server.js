5b32ce2b415ca1312f2c5b8083d24c5455c1a940
Mnarvaez98 <mnarvaez24@uan.edu.co>
2021-11-03 10:57:32 -0500

    flag/ try to fixed db connection with .env varibles

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 app = express();
-port = 3000;
 app.use(express.json());
 app.use(cors());
-
 app.use("/voces", require("./src/routes/voces"));
 app.use("/avatar", require("./src/routes/avatar"));
 app.use("/preguntas", require("./src/routes/preguntas"));
@@ -16,9 +14,8 @@ app.use("/auth", require("./src/routes/auth"));
 
 // data base connection
 async function connect() {
-  const db = "mongodb+srv://ejemplo:ejemplo@cluster0.pwo3c.mongodb.net/bancovoces?retryWrites=true&w=majority"; 
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -31,7 +28,7 @@ async function connect() {
   }
 }
 
-connect();
-app.listen(port, () => {
-  console.log("Servidor: ", port);
+app.listen(process.env.PORT, () => {
+  console.log("Servidor: ", process.env.PORT);
 });
+connect();

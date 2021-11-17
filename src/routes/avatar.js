fd6e980024c42747ba1d2a6c1704390a263b4b12
Mnarvaez98 <mnarvaez24@uan.edu.co>
2021-11-17 17:32:31 -0500

    feat/ middleware for admin implemented and ready for be deployed

--- src/routes/avatar.js
+++ src/routes/avatar.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const avatar = require('../controllers/avatar');
+const avatar = require("../controllers/avatar");
 const mailer = require("../controllers/mailer");
 const isAdmin = require("../middlewares/admin");
 
-router.get("/streak/:id" , avatar.getStreakDays);
+router.get("/streak/:id", avatar.getStreakDays);
 
-router.get("/listenreg/:id/:userId" , avatar.listenReg); 
+router.get("/listenreg/:id/:userId", avatar.listenReg);
 
-router.get("/listen/:userId" , avatar.listen);
+router.get("/listen/:userId", avatar.listen);
 
-router.get("/regquestions/:id/:userId" , avatar.regQuestion);
+router.get("/regquestions/:id/:userId", avatar.regQuestion);
 
-router.get("/resolved/:userId" , avatar.resolve);
+router.get("/resolved/:userId", avatar.resolve);
 
 router.get("/progress/:userId", avatar.progress);
 
@@ -26,11 +26,10 @@ router.post("/register", avatar.register);
 
 router.post("/sendEmail/:id", mailer.sendEmail);
 
-router.put("/saveProgress/:userId",avatar.saveProgress);
+router.put("/saveProgress/:userId", avatar.saveProgress);
 
-router.get("/getActual/:userId",avatar.getActual);
+router.get("/getActual/:userId", avatar.getActual);
 
-router.get("/getUsers/:userId",avatar.getUsers);
-// isAdmin.isAdmin,
+router.get("/getUsers/:userId", isAdmin.isAdmin, avatar.getUsers);
 
 module.exports = router;
